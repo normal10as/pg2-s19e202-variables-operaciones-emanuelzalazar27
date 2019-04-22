8d76909d1beea54fb4511d531c874da21d15f71e
Emanuel <ema008@hotmail.com>
2019-04-22 17:54:49 -0300

    Ejercicio nro 1 Guia Practica Nro2

---
+++ Variables/Variables_consola/Program.vb
@@ -0,0 +1,19 @@
+Option Strict On
+Imports System
+Module Variables
+    Sub Main()
+        'declaration'
+        Dim nombre, apellido As String
+        Dim fechaNacimiento As Date
+
+        Console.WriteLine("Ingrese su Nombre")
+        nombre = Console.ReadLine()
+        Console.WriteLine("Ingrese su Apellido")
+        apellido = Console.ReadLine()
+        Console.WriteLine("Ingrese su Fecha de nacimiento")
+        fechaNacimiento = Convert.ToDateTime(Console.ReadLine())
+
+        Console.WriteLine("Nombre: " & nombre & vbNewLine & "Apellido :" & apellido)
+        Console.WriteLine("Fecha de Nacimiento: " & fechaNacimiento)
+    End Sub
+End Module
