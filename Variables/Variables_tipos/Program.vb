d47b0ce8f4e8d721caf29698ab35d88411a9ec95
Emanuel <ema008@hotmail.com>
2019-05-05 18:22:07 -0300

    1.2

---
+++ Variables/Variables_tipos/Program.vb
@@ -0,0 +1,25 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim nombre, lugarnacimiento, nombreCalle As String
+        Dim documento As UInteger
+        Dim cantHabitantes As Integer
+        Dim fechaNacimiento As Date
+        Dim pesoEnKg, altura As Byte
+        Dim temperatura As SByte
+        Dim alturaCalle, profDeMar, distanciaEnKm As UShort
+        Dim montoArticulo As Decimal
+        Dim esArgentino As String
+
+        nombre = "Emanuel"
+        lugarnacimiento = "Puerto Iguazu"
+        nombreCalle = "Calle Pepito"
+        documento = 55555555
+        cantHabitantes = 250000
+        altura = 185
+        temperatura = 99.5
+        distanciaEnKm = 300.6
+
+    End Sub
+End Module
---
+++ Variables_tipos/Variables_tipos/stdafx.h
@@ -0,0 +1,3 @@
+#pragma once
+
+// TODO: hacer referencia aquí a los encabezados adicionales que el programa necesita
